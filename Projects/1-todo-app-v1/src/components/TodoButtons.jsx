b6cae0cc9bab182faab8ebea610a3005c1152573
Gilpix <kulsinggh@gmail.com>
2023-12-25 19:05:29 -0500

    add TodoButton component

---
+++ Projects/1-todo-app-v1/src/components/TodoButtons.jsx
@@ -0,0 +1,19 @@
+import { TodoItem } from "./TodoItem";
+
+let TodoButton = ({ buttonType }) => {
+  return (
+    <div className="col-2">
+      <button
+        className={
+          buttonType == "add"
+            ? "btn btn-success todo-btn"
+            : "btn btn-danger todo-btn"
+        }
+      >
+        {buttonType == "add" ? "Add" : "Delete"}
+      </button>
+    </div>
+  );
+};
+
+export default TodoButton;
