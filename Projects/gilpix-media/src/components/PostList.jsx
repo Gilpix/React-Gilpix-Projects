ae02d32a00f942df9fc3c9f8b933cf6c20b3ca53
Gilpix <kulsinggh@gmail.com>
2024-01-02 00:43:54 -0500

    Use Context to get PostList

--- Projects/gilpix-media/src/components/PostList.jsx
+++ Projects/gilpix-media/src/components/PostList.jsx
@@ -1,12 +1,17 @@
 import Post from "./Post";
+import { useContext } from "react";
+import { PostList as PostListData } from "../store/post-list-store";
 
 const PostList = () => {
+  const { postList } = useContext(PostListData);
   return (
-    <>
-      <Post></Post>
-      <Post></Post>
-      <Post></Post>
-    </>
+    <div className="container mt-4">
+      <div className="row">
+        {postList.map((post) => {
+          return <Post key={post.id} post={post}></Post>;
+        })}
+      </div>
+    </div>
   );
 };
 
