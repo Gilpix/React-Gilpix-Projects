d4f703ee69554c876ff76d1407a883230519b8b1
Gilpix <kulsinggh@gmail.com>
2024-01-10 23:15:31 -0500

    add inputFunctionality&Privacy comp. - RedxCounter

--- Projects/Counter-React-Redux/src/App.jsx
+++ Projects/Counter-React-Redux/src/App.jsx
@@ -4,15 +4,19 @@ import Header from "./components/Header";
 import DisplayCounter from "./components/DisplayCounter";
 import Controls from "./components/Controls";
 import Container from "./components/Container";
+import { useSelector } from "react-redux";
+import CounterPrivacy from "./components/CounterPrivacy";
 
 function App() {
+  const counterPrivacy = useSelector((state) => state.privacy);
+
   return (
     <>
       <div className="px-4 py-5 my-5 text-center">
         <Container>
           <Header></Header>
           <div className="col-lg-6 mx-auto">
-            <DisplayCounter></DisplayCounter>
+            {counterPrivacy ? <CounterPrivacy /> : <DisplayCounter />}
             <Controls></Controls>
           </div>
         </Container>
--- Projects/Counter-React-Redux/src/store/index.jsx
+++ Projects/Counter-React-Redux/src/store/index.jsx
@@ -2,15 +2,26 @@ import { createStore } from "redux";
 
 const INITIAL_VALUE = {
   counter: 0,
+  privacy: false,
 };
 
 const counterReducer = (store = INITIAL_VALUE, action) => {
   let newStore = store;
   if (action.type == "INCREMENT") newStore = { counter: store.counter + 1 };
   else if (action.type == "DECREMENT")
+    newStore = { ...newStore, counter: store.counter - 1 };
+  else if (action.type == "ADD_INPUT")
     newStore = {
-      counter: store.counter - 1,
+      ...newStore,
+      counter: store.counter + parseInt(action.payload.value),
     };
+  else if (action.type == "SUBTRACT_INPUT")
+    newStore = {
+      ...newStore,
+      counter: store.counter - parseInt(action.payload.value),
+    };
+  else if (action.type == "PRIVACY_TOGGLE")
+    newStore = { ...newStore, privacy: !store.privacy };
   return newStore;
 };
 
